07294ee24b2903463be0df228248330b7d4b66d9
Artem Tsyganov <asterionblaze@gmail.com>
2025-04-27 22:00:14 +0300

    test8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <>
-            <div className="[font-weight:453] text-4xl">Next Up</div>
+            <div className="[font-weight:453] text-4xl">Next up</div>
         </>
     );
 }
